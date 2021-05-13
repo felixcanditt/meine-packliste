561ee8be9a3112fc64edaff0feb2ca38e9587311
Felix Canditt <felix.canditt@gmail.com>
2021-05-13 17:42:39 +0200

    introduced useState, backend list, display of list on screen, added checkboxes with functionality

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import styled from 'styled-components/macro';
 import { useState } from 'react';
 
 export default function App() {
-  //const { listItems, setListItems } = useState([]);
-
-  //test: geht was anderes?
-  function onSubmitForm(event) {
-    event.preventDefault(); // automatisches Neuladen der Seite verhindern
-    const myForm = event.target; // Standard Befehl zum Auswählen des Formulars
-    const inputField = myForm.eingabefeld;
-    console.log(inputField.value);
-    myForm.reset(); // Eingabezeile leeren
-    inputField.focus(); // Cursor landet automatisch in Eingabezeile
-  }
+  const [packingListItems, updatePackingListItems] = useState([]);
+  // Variable, Methode zum Aktualisieren der Variable
+  // leeres Array am Anfang
+  console.log(packingListItems);
 
   return (
     <main>
       <Headline>Meine Packliste</Headline>
       <form
-        onSubmit={onSubmitForm}
+        onSubmit={addNewItemToPackingList}
         /* kurz für:
          {(event) => {
             onSubmitForm(event);
@@ -28,15 +21,54 @@ export default function App() {
         <input type="text" name="eingabefeld" />
         <button>zur Liste hinzufügen</button>
       </form>
-      <ul>
-        <li>Zahnbürste</li>
-      </ul>
+      <ul>{showPackingListOnScreen(packingListItems)}</ul>
     </main>
   );
+
+  function addNewItemToPackingList(event) {
+    event.preventDefault(); // automatisches Neuladen der Seite verhindern
+    const myForm = event.target; // Standard Befehl zum Auswählen des Formulars
+    const inputField = myForm.eingabefeld;
+    const newItem = {
+      name: inputField.value,
+      isPacked: false
+    };
+    updatePackingListItems([newItem, ...packingListItems]); // neues item auf die Liste setzen
+
+    myForm.reset(); // Eingabezeile leeren
+    inputField.focus(); // Cursor landet automatisch in Eingabezeile
+  }
+
+  function showPackingListOnScreen(itemsList) {
+    // warum hier normale Klammer () ?
+    return itemsList.map((item) => (
+      <li>
+        <label>
+          <input
+            type="checkbox"
+            checked={item.isPacked}
+            onChange={(event) => changePackedStatusOfItem(item)}
+          />
+          {item.name}
+        </label>
+      </li>
+    ));
+  }
+
+  function changePackedStatusOfItem(itemWithNewPackedStatus) {
+    // warum hier geschweifte Klammer {} ?
+    const packingListWithNewPackedStatus = packingListItems.map((item) => {
+      if (item.name === itemWithNewPackedStatus.name) {
+        item.isPacked = !item.isPacked;
+      }
+      return item;
+    });
+    updatePackingListItems(packingListWithNewPackedStatus);
+  }
 }
 
 const Headline = styled.h1`
   xcolor: hotpink;
 `;
 
-//    inputField.value muss in Liste: virtuell und auf Bildschirm
+//    liste auf bildschirm malen
