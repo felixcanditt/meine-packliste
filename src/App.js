2ec272c3f1cdcefef2877479871d40c63461ab82
Felix Canditt <felix.canditt@gmail.com>
2021-05-13 15:20:06 +0200

    console(log) new item works, ready for useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import styled from 'styled-components/macro';
+import { useState } from 'react';
 
 export default function App() {
+  //const { listItems, setListItems } = useState([]);
+
+  //test: geht was anderes?
+  function onSubmitForm(event) {
+    event.preventDefault(); // automatisches Neuladen der Seite verhindern
+    const myForm = event.target; // Standard Befehl zum Auswählen des Formulars
+    const inputField = myForm.eingabefeld;
+    console.log(inputField.value);
+    myForm.reset(); // Eingabezeile leeren
+    inputField.focus(); // Cursor landet automatisch in Eingabezeile
+  }
+
   return (
     <main>
       <Headline>Meine Packliste</Headline>
-      <form onSubmit="">
-        <input type="text" name="inputFieldForNewItem" />
+      <form
+        onSubmit={onSubmitForm}
+        /* kurz für:
+         {(event) => {
+            onSubmitForm(event);
+          }}
+          */
+      >
+        <input type="text" name="eingabefeld" />
         <button>zur Liste hinzufügen</button>
       </form>
+      <ul>
+        <li>Zahnbürste</li>
+      </ul>
     </main>
   );
 }
@@ -15,3 +38,5 @@ export default function App() {
 const Headline = styled.h1`
   xcolor: hotpink;
 `;
+
+//    inputField.value muss in Liste: virtuell und auf Bildschirm
